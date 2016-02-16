350bdcec08268b22b1cd8fbf9bbec8ac9ac8a066
Saurabh Datar <sdatar2@stanford.edu>
2016-02-15 22:13:40 -0800

    Top 5 names with letter x, included comments

--- exercises/0013-sorted-names/d.py
+++ exercises/0013-sorted-names/d.py
@@ -7,19 +7,23 @@ textfile = basename(url)
 boys = []
 girls = []
 
+#generic function to check if letter is present
 def check_for_letter(element, letter):
 	return letter.lower() in element or letter.upper() in element
 
+#function to print x number of times
 def print_x_times(array, count):
 	for i in range(count):
 		name_length = len(array[i][0])
 		print(str(i+1) + '.', array[i][0], str(array[i][2]).rjust(24 - name_length - 3))
 
+#open file, read its contents
 with open('tempdata/' + textfile, 'r') as baby_file:
 	for line in baby_file:
 		name, sex, babies = line.strip().split(',')
 		row = [name, sex, babies]
-		
+
+		#check if boy or girl, then check if letter is present		
 		if row[1] == 'F':
 			if check_for_letter(row[0], 'x'):
 				girls.append(row)
@@ -27,6 +31,7 @@ with open('tempdata/' + textfile, 'r') as baby_file:
 			if check_for_letter(row[0], 'x'):
 				boys.append(row)
 
+#print result
 print('Female')
 print_x_times(girls, 5)
 
