5e88c95e532b64de0aa7dca72032600fe4668ec0
Saurabh Datar <sdatar2@stanford.edu>
2016-02-15 22:10:57 -0800

    10 most popular names

--- exercises/0013-sorted-names/b.py
+++ exercises/0013-sorted-names/b.py
@@ -4,14 +4,17 @@ from operator import itemgetter
 url = 'http://stash.compciv.org/ssa_baby_names/ssa-babynames-nationwide-2014.txt'
 textfile = basename(url)
 
+#open file
 baby_file = open('tempdata/' + textfile, 'r')
 baby_list = []
 
+#read file contents and make a list of each row of data
 for line in baby_file:
 	name, sex, babies = line.strip().split(',')
 	temp = [name, sex, int(babies)]
 	baby_list.append(temp)
 
+#sort by baby count descending
 answer = sorted(baby_list, key=itemgetter(2), reverse=True)
 
 for i in range(10):
