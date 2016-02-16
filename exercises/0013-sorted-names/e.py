3aa53b86d4ae335156b9e5dcd6aff9c4ca970f57
Saurabh Datar <sdatar2@stanford.edu>
2016-02-15 22:05:02 -0800

    Numbers and total baby count is different

--- exercises/0013-sorted-names/e.py
+++ exercises/0013-sorted-names/e.py
@@ -5,14 +5,15 @@ textfile = basename(url)
 
 baby_list = []
 
+#key function to sort by baby count
 def sort_by_number(x):
 	return x[1]
 
+#calculate percentages, given a percentage bracket
 def percent_bracket(start, end, array):
 	bracket_babies = 0
 	for i in range(start-1, end):
 		bracket_babies += array[i][1]
-
 	percent = (bracket_babies/total_babies) * 100
 	print('Names', str(start), 'to', str(end) + ':', round(percent,1))
 
@@ -25,6 +26,7 @@ with open('tempdata/' + textfile, 'r') as baby_file:
 		total_babies += int(babies)
 		baby_list.append(row)
 
+#sorted array by baby count descending
 a = sorted(baby_list, key=sort_by_number, reverse=True)
 
 percent_bracket(1, 10, a)
