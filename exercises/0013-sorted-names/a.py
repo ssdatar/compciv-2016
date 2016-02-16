4bb82d69cff6b9230f8f25dc1b3d4d7123efaf1a
Saurabh Datar <sdatar2@stanford.edu>
2016-02-15 22:10:14 -0800

    Printed number of characters

--- exercises/0013-sorted-names/a.py
+++ exercises/0013-sorted-names/a.py
@@ -2,13 +2,16 @@ import os
 import requests
 from os.path import basename
 
+#make the directory
 os.makedirs('tempdata', exist_ok = True)
 url = 'http://stash.compciv.org/ssa_baby_names/ssa-babynames-nationwide-2014.txt'
 
+#download content
 response = requests.get(url)
 content = response.text
 base = basename(url)
 
+#open file
 baby_name = os.path.join('tempdata', base)
 baby_file = open(baby_name, 'wb')
 baby_file.write(response.content)
