35712a22dd061a15e06de502dab3e2c6620b0464
Saurabh Datar <sdatar2@stanford.edu>
2016-03-06 23:04:37 -0800

    Fixed the counts

--- exercises/0020-gender-detector/e.py
+++ exercises/0020-gender-detector/e.py
@@ -6,28 +6,27 @@ girls = 0
 boys  = 0
 
 with open('tempdata/yob2014.txt', 'r') as file_2014:
-	for line in file_2014:
-		name, gender, babies = line.split(',')
-		
-		if not names_count.get(name):
-			names_count[name] = [0, 0]
+    for line in file_2014:
+        name, gender, babies = line.split(',')
+        
+        if not names_count.get(name):
+            names_count[name] = [0, 0]
 
-		if gender == 'F':
-			names_count[name][0] += int(babies)
-			girls += int(babies)
+        if gender == 'F':
+            names_count[name][0] += int(babies)
+            girls += int(babies)
 
-		else:
-			names_count[name][1] += int(babies)
-			boys += int(babies)
+        else:
+            names_count[name][1] += int(babies)
+            boys += int(babies)
 
 unique_count = {'M': 0, 'F': 0}
 
-
 for i in names_count:
-	if names_count[i][0] > 0:
-		unique_count['F'] += 1
-	else:
-		unique_count['M'] += 1
+    if names_count[i][0] > 0:
+        unique_count['F'] += 1
+    if names_count[i][1] > 0:
+        unique_count['M'] += 1
 
 print('Total: ', len(names_count), 'unique names for ', boys + girls, ' babies')
 print('    M: ', unique_count['M'], 'unique names for ', boys, ' babies')
